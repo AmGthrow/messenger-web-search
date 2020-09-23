1d2bd476cefd3d5e755adb11bbbcc7f7c436b237
potato874 <jethro.modequillo@mensaphilippines.org>
2020-09-23 08:47:35 +0800

    Cleaned up messenger stuff

--- app.py
+++ app.py
@@ -62,138 +62,28 @@ def verify_fb_token(token_sent):
 def handle_postback(recipient_id, message, profile):
     payload = message.get('payload')
     bot.send_action(recipient_id, 'mark_seen')
-    if payload == 'get_started':
-        take_thread_control(recipient_id)
-        response = "Hey there, %s! Welcome to Seven Chunks ☺️👋" %profile.get('first_name')
-        bot.send_text_message(recipient_id, response)
 
-        typing(recipient_id)
-        response = "You're currently talking to a chatbot, so the order process " + \
-        "is mostly automated. However, you can also talk to a human whenever you like."
-        bot.send_text_message(recipient_id, response)
-         
-        typing(recipient_id)
-        response = "How may I help you today?"
-        quick_replies = [{
-            "content_type" : "text",
-            "title" : "Order Now",
-            "payload" : "order_now"
-        },
-        {
-            "content_type" : "text",
-            "title" : "Talk to Someone",
-            "payload" : "talk_human"
-        }]
-        bot.send_quick_reply(recipient_id, response, quick_replies)
-        return
-    elif payload == 'main_menu':
-        response = "Anything else I can do for you?"
-        quick_replies = [{
-            "content_type" : "text",
-            "title" : "Order Now",
-            "payload" : "order_now"
-        },
-        {
-            "content_type" : "text",
-            "title" : "Talk to Someone",
-            "payload" : "talk_human"
-        }]
-        bot.send_quick_reply(recipient_id, response, quick_replies)
-        return
-
-    elif payload == 'order_now':
-        take_thread_control(recipient_id)
-        response = "Sure thing. What would you like?"
-        quick_replies = [{
-            "content_type" : "text",
-            "title" : "Beef Bibimbap",
-            "payload" : "beef_bibimbap"
-        },
-        {
-            "content_type" : "text",
-            "title" : "Beef Gyudon",
-            "payload" : "beef_gyudon"
-        }]
-        bot.send_quick_reply(recipient_id, response, quick_replies)
-        return
-
-    elif payload == 'talk_human':
-        response = "Sure thing 🙂 Someone's been notified and they'll will be with you shortly. Feel free to leave them a message!"
-        bot.send_text_message(recipient_id, response)
-        response = "If you want to continue talking to the bot again, press either \"Start Over\" or \"Place an Order\" from the menu buttons below."
-        bot.send_text_message(recipient_id, response)
-        response = requests.post(f"https://graph.facebook.com/v8.0/me/pass_thread_control",
-        data=[
-            ('recipient', "{'id':%s}"%recipient_id),
-            ('target_app_id', "263902037430900"),
-            ('access_token', ACCESS_TOKEN) 
-        ])
-        print(response.json())
-        #TODO: message someone HAHAHAHA
-        return
-    else:
-        response = "I don't have a protocol for that postback {%s} yet"%payload
-        bot.send_text_message(recipient_id, response)
+    response = "I don't have a protocol for that postback {%s} yet"%payload
+    bot.send_text_message(recipient_id, response)
 
 
 def handle_quick_reply(recipient_id, message, profile):
     payload = message['quick_reply'].get('payload')
-    if payload == 'order_now':
-        handle_postback(recipient_id,{'payload':'order_now'},profile)
 
-    elif payload in MENU_ITEMS:
-        users[recipient_id].add_order(message.get('text'))
-        response = f"Your current order is:\n{users[recipient_id].get_order()}"
-        bot.send_text_message(recipient_id, response)
-        response = "Anything else?"
-        quick_replies = [{
-            "content_type" : "text",
-            "title" : "Beef Bibimbap",
-            "payload" : "beef_bibimbap"
-        },
-        {
-            "content_type" : "text",
-            "title" : "Beef Gyudon",
-            "payload" : "beef_gyudon"
-        },
-        {
-            "content_type" : "text",
-            "title" : "Place Order",
-            "payload" : "place_order"
-        }        ]
-        bot.send_quick_reply(recipient_id, response, quick_replies)
-
-    elif payload == 'place_order':
-        bot.send_text_message(recipient_id, "Great! You just placed an order.")
-        bot.send_text_message(recipient_id, "This is as far as the bot goes right now, I still need to collect your contact details (I haven't implemented that yet).")
-        users[recipient_id].clear_order()
-        handle_postback(recipient_id,{'payload':'main_menu'},profile)
-        #TODO: Continue processing the order
-
-    elif payload == 'talk_human':
-        handle_postback(recipient_id, {'payload':'talk_human'}, 'profile')
-
-
-    else:
-        response = f"You just sent me the quick reply: {payload}"
-        bot.send_text_message(recipient_id, response)
+    response = "I don't have a protocol for that quick reply {%s} yet"%payload
+    bot.send_text_message(recipient_id, response)
 
 
 def handle_message(recipient_id, message, profile):
     if message.get('text'):
         # if the user sent a message containing text
-        response = f"I'm sorry, I don't recognize the command \"{message.get('text')}\"."
+        response = "I'll try running a google search."
         bot.send_text_message(recipient_id, response)
-        typing(recipient_id)
-        response = "You're talking to a chatbot right now, but you can talk to someone from " + \
-                   "our team at any time by pressing the \"Talk to Someone\" button."
-        bot.send_text_message(recipient_id, response)
-        handle_postback(recipient_id, {'payload':'main_menu'}, 'profile')
 
-    if message.get('attachments'):
+    elif message.get('attachments'):
         # if the user sent a message containing an attachment
-        response = "Thanks for sending this! Someone from our team will come and review it."
-        handle_postback(recipient_id, {'payload':'main_menu'}, 'profile')
+        response = "Sorry, I can't process images and attachments yet."
+        bot.send_text_message(recipient_id, response)
 
 
 def typing(recipient_id, wait=0.5, type=1.5):
@@ -201,41 +91,6 @@ def typing(recipient_id, wait=0.5, type=1.5):
         bot.send_action(recipient_id, 'typing_on')
         time.sleep(type)
         bot.send_action(recipient_id, 'typing_off')
-
-
-class Customer():
-    menu = {
-        "Beef Bibimbap" : 200,
-        "Beef Gyudon" : 150
-    }
-    def __init__(self, psid):
-        self.psid = psid
-        self.orders = {}
-
-    def add_order(self, order):
-        try:
-            self.orders[order] += 1
-        except KeyError:
-            self.orders[order] = 1
-
-    def get_order(self):
-        order_summary = []
-        for (order, num_order) in self.orders.items():
-            if num_order != 0:
-                order_summary.append(f'{num_order}x {order}')
-        return('\n'.join(order_summary))
-    
-    def clear_order(self):
-        self.orders = {}
-
-    def set_address(self, address):
-        self.address = address
-
-    def get_address(self):
-        return self.address
-
-    def get_total(self):
-        return sum([menu[order] * num_order for (order, num_order) in self.orders.items()])
 
 
 def take_thread_control(recipient_id):
